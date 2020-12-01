d89b05ba21dc265674f43e7df51f73a21fb88a6b
Rafael Silva <rafaelvds@gmail.com>
2020-12-01 17:20:14 -0300

    solving yesterday's and today's exercises

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+gem 'dotenv-rails', groups: [:development, :test]
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rspec-rails', '4.0.0.beta3', group: [ :test ]
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,10 @@ require("channels")
 
 import 'bootstrap';
 
+import improveDropdown from '../components/select';
+
+improveDropdown();
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
